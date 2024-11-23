9a8449beb307845e420359c0d27c069b744f4a89
Hariharan Mohan <haranhari350@gmail.com>
2024-11-23 12:40:43 -0600

    css fix

--- src/MainScreen.jsx
+++ src/MainScreen.jsx
@@ -12,6 +12,16 @@ export default function MainScreen({ handleClick, userInput, data }) {
         </button>
       </div>
       <div className="container">
+        {/* <div className="search">
+          <input
+            type="text"
+            placeholder="Enter Location..."
+            ref={userInput}
+          ></input>
+          <button className="btn" onClick={handleClick}>
+            >
+          </button>
+        </div> */}
         <div className="top">
           <div className="location">
             <p>{data.name}</p>
@@ -23,26 +33,26 @@ export default function MainScreen({ handleClick, userInput, data }) {
             {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
-        {data.name !== undefined && (
-          <div className="bottom">
-            <div className="feels">
-              {data.main ? (
-                <p className="bold">{data.main.feels_like.toFixed()}°F</p>
-              ) : null}
-              <p className="bold">Feels Like</p>
-            </div>
-            <div className="humidity">
-              {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
-              <p className="bold">Humidity</p>
-            </div>
-            <div className="wind">
-              {data.wind ? (
-                <p className="bold">{data.wind.speed.toFixed()} MPH</p>
-              ) : null}
-              <p className="bold">Wind Speed</p>
-            </div>
+        {/* {data.name !== undefined && ( */}
+        <div className="bottom">
+          <div className="feels">
+            {data.main ? (
+              <p className="bold">{data.main.feels_like.toFixed()}°F</p>
+            ) : null}
+            <p className="bold">Feels Like</p>
           </div>
-        )}
+          <div className="humidity">
+            {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
+            <p className="bold">Humidity</p>
+          </div>
+          <div className="wind">
+            {data.wind ? (
+              <p className="bold">{data.wind.speed.toFixed()} MPH</p>
+            ) : null}
+            <p className="bold">Wind Speed</p>
+          </div>
+        </div>
+        {/* )} */}
       </div>
     </div>
   );
