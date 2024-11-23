13d8ddd33ca26e577090c5a0882fe424063de5cb
Hariharan Mohan <haranhari350@gmail.com>
2024-11-23 09:39:02 -0600

    Cleaned up the code base

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
 import { useEffect, useRef, useState } from "react";
-
-// const location = "Dallas";
+import MainScreen from "./MainScreen.jsx";
 
 function App() {
   // const [loading, isLoading] = useState(false);
-  // const [error, setError] = useState(null);
+  const [error, setError] = useState(null);
   const [data, setData] = useState({});
-  const [location, setLocation] = useState("");
 
-  const keystroke = useRef();
+  const [location, setLocation] = useState("");
+  const userInput = useRef();
 
   function handleClick() {
-    const typedLoc = keystroke.current.value;
-    setLocation(typedLoc);
+    const inputLocation = userInput.current.value;
+    setLocation(inputLocation);
+    userInput.current.value = "";
   }
-  // const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=aa601f7e782dc02a2ada20a7a1d64043`;
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=aa601f7e782dc02a2ada20a7a1d64043`;
 
   useEffect(() => {
+    if (!location) return;
     const searchLocation = async () => {
       try {
         const response = await fetch(url);
-
         if (!response.ok) {
           throw new Error("Failed to fetch data");
         }
@@ -28,7 +29,7 @@ function App() {
         setData(responseData);
         console.log(responseData);
       } catch (error) {
-        setError(error);
+        setError(error.message);
       }
     };
     searchLocation();
@@ -36,45 +37,7 @@ function App() {
 
   return (
     <>
-      <div className="app">
-        <div className="search">
-          <input
-            type="text"
-            placeholder="Enter Location..."
-            ref={keystroke}
-          ></input>
-          <button className="btn" onClick={handleClick}>
-            click
-          </button>
-        </div>
-        <div className="container">
-          <div className="top">
-            <div className="location">
-              <p>{data.name}</p>
-            </div>
-            <div className="temp">
-              <h1>60 F</h1>
-            </div>
-            <div className="description">
-              <p>clouds</p>
-            </div>
-          </div>
-          <div className="bottom">
-            <div className="feels">
-              {/* <p>{data.main.feels_like}</p> */}
-              <p className="bold">Feels Like</p>
-            </div>
-            <div className="humidity">
-              {/* <p>{data.main.humidity}%</p> */}
-              <p className="bold">Humidity</p>
-            </div>
-            <div className="wind">
-              {/* <p>{data.wind.speed} MPH</p> */}
-              <p className="bold">Wind Speed</p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <MainScreen handleClick={handleClick} userInput={userInput} data={data} />
     </>
   );
 }
---
+++ src/MainScreen.jsx
@@ -0,0 +1,49 @@
+export default function MainScreen({ handleClick, userInput, data }) {
+  return (
+    <div className="app">
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Enter Location..."
+          ref={userInput}
+        ></input>
+        <button className="btn" onClick={handleClick}>
+          click
+        </button>
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp.toFixed()}°F</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+        {data.name !== undefined && (
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? (
+                <p className="bold">{data.main.feels_like.toFixed()}°F</p>
+              ) : null}
+              <p className="bold">Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
+              <p className="bold">Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? (
+                <p className="bold">{data.wind.speed.toFixed()} MPH</p>
+              ) : null}
+              <p className="bold">Wind Speed</p>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
